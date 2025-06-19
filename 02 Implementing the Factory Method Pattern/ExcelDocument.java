d4dc8526d57c41df756f0ef6db1cd4bb2a363026
Divyansh04-kt <divyanshkatakwar8@gmail.com>
2025-06-19 23:07:07 +0530

    Create ExcelDocument.java

---
+++ 02 Implementing the Factory Method Pattern/ExcelDocument.java
@@ -0,0 +1,13 @@
+package com.factory.design;
+
+//ExcelDocument.java
+public class ExcelDocument implements Document {
+ 
+ public void open() {
+     System.out.println("Opening Excel document.");
+ }
+
+ public void close() {
+     System.out.println("Closing Excel document.");
+ }
+}
